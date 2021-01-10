c5542bb9109a320c507c06c68dc0091b99dada90
AriShaked <arishaked10@gmail.com>
2021-01-10 15:09:04 +0200

    automatic_fight

--- Controllers/FightController.cs
+++ Controllers/FightController.cs
@@ -22,6 +22,18 @@ namespace dotnet_rpg.Controllers
             return Ok(await _fightService.WeaponAttack(request));
         }
 
+        [HttpPost("Skill")]
+        public async Task<IActionResult> SkillAttack(SkillAttackDto request)
+        {
+            return Ok(await _fightService.SkillAttack(request));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Fight(FightRequestDto request)
+        {
+            return Ok(await _fightService.Fight(request));
+        }
+
     }
 }
 
